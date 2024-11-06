179b2fb3603c0489596a68fe6273b7852b5d4e92
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2024-11-06 22:11:28 +0000

    Update metadata

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60bf2a8832985d5184f1c079d1823a44cb9efa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37918c882daf32d9e76d8c03804cf87740d72475")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
